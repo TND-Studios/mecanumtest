5536222ca65095d25b4f48821c0482720ed8d453
Mystical-Codez <37031914+Mystical-Codez@users.noreply.github.com>
2021-03-24 23:12:27 -0700

    Framework Part 2 - improved AutonomousSegment constructing

--- src/main/java/frc/robot/AutonomousSegment.java
+++ src/main/java/frc/robot/AutonomousSegment.java
@@ -12,12 +12,18 @@ public class AutonomousSegment {
     public boolean angleGreater; 
 
     public boolean ignoreAngle = false; 
-    public AutonomousSegment(double l, double a, AutonomousSegment prev, double[] s, boolean distanceGreater, boolean angleGreater) {
-        length = l;
-        angleChange = a;
+    public AutonomousSegment(double len, double ang, AutonomousSegment prev, double[] s, boolean distanceGreater, boolean angleGreater) {
+        length = len;
+        angleChange = ang;
 
-        totalLength = prev.totalLength + length;
-        totalAngleChange = prev.angleChange + angleChange;
+        if (prev != null) {
+            totalLength = prev.totalLength + length;
+            totalAngleChange = prev.angleChange + angleChange;
+        } else {
+            totalLength = length;
+            totalAngleChange = angleChange;
+        }
+        
 
         speeds = s;
 
@@ -27,21 +33,7 @@ public class AutonomousSegment {
         if (speeds[0] == speeds[1]) ignoreAngle = true;
     }
 
-    public AutonomousSegment(double l, double a, double[] s, boolean distanceGreater, boolean angleGreater) {
-        length = l;
-        angleChange = a;
 
-        totalLength = length;
-        totalAngleChange = angleChange;
-
-        speeds = s;
-
-        this.distanceGreater = distanceGreater;
-        this.angleGreater = angleGreater;
-
-        if (speeds[0] == speeds[1]) ignoreAngle = true;
-
-    }
 
 
     public void SetSpeeds(Controller c) {
@@ -57,7 +49,7 @@ public class AutonomousSegment {
         }
 
         if (ignoreAngle) return true; 
-        
+
         if (angleGreater) {
             if (angle < totalAngleChange) return false;
         } else {
