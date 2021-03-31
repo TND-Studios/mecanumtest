baedaf399e2ba26b2cbb3e8516cfda76dcbc2704
Mystical-Codez <37031914+Mystical-Codez@users.noreply.github.com>
2021-03-30 20:53:26 -0700

    removed unnecessary stuff, added base code

--- src/main/java/frc/robot/Controller.java
+++ src/main/java/frc/robot/Controller.java
@@ -17,196 +17,38 @@ public class Controller{
     //main controller
     public XboxController xcontroller;
     private Wheels wheels;
-    //private int intakeVal;
-    //    CONVEYOR CODE DELETED (????)
-
-    private Intake intake;
-    private int intakePort = 2;
-    
-    private int hookPort = 9;
-    private int hookPortTwo = 10;
-    private int shooterPortOne = 7;
-    private int shooterPortTwo = 4;
-
-
-    private Shooter shooter;
-
-    private AHRS ahrs;
-    private AnalogInput m_ultrasonic;
-
-    private Boolean hookUp; 
-    private HookExtension hook;
-
-    private VisionComp visionComp;
-
-    private DigitalInput dioOne, dioTwo;
-
-
-    private ChallengeOne cOne;
-    private ChallengeTwo cTwo;
-    private ChallengeThree cThree;
-    private ChallengeFour cFour;
-    private ChallengeFive cFive;
-    //-1 = testing, 1 = challenge one, 2 = challenge 2... 5 = challenge 5
-    private int challengeNumber = 4; 
-
 
 
     public void controllerInit()
     {
-        ahrs = new AHRS(SPI.Port.kMXP);
-        m_ultrasonic = new AnalogInput(0);
+
       
   
         // fL, fR, bL, bR
-        wheels = new Wheels(30, 31, 32, 33);
+        wheels = new Wheels(6, 8, 3, 1);
         xcontroller = new XboxController(0);
 
-        shooter = new Shooter(shooterPortOne, shooterPortTwo);
-        visionComp = new VisionComp();
 
-
-        dioOne = new DigitalInput(0);
-        dioTwo = new DigitalInput(1);
-
-        intake = new Intake(intakePort);
-
-        hook = new HookExtension(hookPort, hookPortTwo);
-
-        hookUp = false;
-
-        cOne = new ChallengeOne(this);
-        cTwo = new ChallengeTwo(this, 1);
-        cThree = new ChallengeThree(this);
-        cFour = new ChallengeFour(this);
-        cFive = new ChallengeFive(this);
 
     }
     
     public void UpdateTeleop() {
-     // get values from the encoder. every 1 represents 1 rotation which is 2 pi r (6 pi) inches
-        //double frontLeftRotations = wheels.getRotations("fL");
-        //double backLeftRotations = wheels.getRotations("bR");
 
-        //get values from the navx gyro to see which angle we are facing
-        //double angleFacing = ahrs.getAngle();
-
-        //get value from the ultrasonic sensor mounted in the front of the robot
-        //double ultrasonicReading = getUltraSonicReading();
-
-
-
-
-        //logic code below
-
-        switch(challengeNumber) {
-            case -1: break;
-            case 1: cOne.UpdateTeleop(); break;
-            case 2: cTwo.UpdateTeleop();  break;
-            case 3: cThree.UpdateTeleop();  break;
-            case 4: cFour.UpdateTeleop();  break;
-            case 5: cFive.UpdateTeleop();  break;
-        }
-        
-        
-
+        wheels.drive(xcontroller.getY(Hand.kLeft), xcontroller.getX(Hand.kLeft), xcontroller.getX(Hand.kRight));
 
     }    
 
-    public void UpdateAutonomous() {
-        // code for all situations - DO NOT COMMENT OUT
-        SmartDashboard.putNumber("Front Left Encoder Distance Travelled (ft)", getDistanceTravelled("fL"));
-        SmartDashboard.putNumber("Back Right Encoder Distance Travelled (ft)", getDistanceTravelled("bR"));
-        SmartDashboard.putNumber("Angle Facing From Gyro (degrees)", getAngleFacing());
-        SmartDashboard.putNumber("Ultrasonic Reading (feet):", getUltraSonicReading() / 12);
-        
-        
-        switch(challengeNumber) {
-            case -1: break;
-            case 1: cOne.UpdateAutonomous(); break;
-            case 2: cTwo.UpdateAutonomous();  break;
-            case 3: cThree.UpdateAutonomous();  break;
-            case 4: cFour.UpdateAutonomous();  break;
-            case 5: cFive.UpdateAutonomous();  break;
-        }
+    public void UpdateAutonomous() { 
 
 
     }
 
-    public int AutonomousTurnCheck() {
-        if (!dioOne.get() && !dioTwo.get()) {
-            visionComp.compute(2); // straight
-            return 2;
-        }
-        
-        if (!dioOne.get()) {
-            visionComp.compute(0); // left
-            return 0;
-        }
-        if (!dioTwo.get()) {
-            visionComp.compute(1); //  right
-            return 1;
-        } 
 
-        if (dioOne.get() && dioTwo.get()) 
-        {
-            visionComp.compute(3); // none
-            return 3;
-        }
-
-        return -1; 
-    }
-
-    public double getUltraSonicReading()
-    {
-        //                return value is in inches
-        return m_ultrasonic.getValue()*0.125f/2.54f;
-    }
-
-    public double getAngleFacing() 
-    {
-        // return value is in degrees
-        return ahrs.getAngle() * -1;
-    }
-    
-    public double getDistanceTravelled(String pos) {
-        // return             rotations * 2 * pi           * r * (1 foot / 12 inches)
-        return wheels.getRotations(pos) * 2 * 3.1415926535 * 3  * 1 / 12;
-    }
-
-    public void setDriveSpeed(double l, double r) {
-        wheels.drive(l, r);
-    }
-
-    public void setIntakeSpeed(double v) {
-        intake.drive(v);
-    }
-
-    public void setShooterSpeed(double v) {
-        shooter.charge(v);
-    }
-
-    public void setFeederSpeed(double v) {
-        shooter.fire(v);
-    }
     
 
-    public double getCompassHeading() {
-        return ahrs.getCompassHeading();
-    }
-
-    public void calibrate() {
-        ahrs.calibrate();
-    }
 
 
-    public void diffDrive(double s1, double s2, Wheels.DriveType driveType) {
-        wheels.diffDrive(s1, s2, driveType);
-    }
-   
-    public void resetDistance() {
-        wheels.resetRotations();
-    }
+
 
     public void inverseWheels() {
         wheels.inverse();
--- src/main/java/frc/robot/Wheels.java
+++ src/main/java/frc/robot/Wheels.java
@@ -1,32 +1,32 @@
 package frc.robot;
 
-import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
 import com.ctre.phoenix.motorcontrol.NeutralMode;
+
+import edu.wpi.first.wpilibj.SpeedController;
 //import com.ctre.phoenix.motorcontrol.ControlMode;
 //import com.ctre.phoenix.motorcontrol.StatusFrameEnhanced;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-//import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import com.ctre.phoenix.motorcontrol.can.*;
 //import java.lang.Math;
 
 public class Wheels {
 
-    public enum DriveType { TANK, ARCADE }
 
-    private WPI_TalonFX frontLeft;
-    private WPI_TalonFX backLeft;
-    private WPI_TalonFX frontRight;
-    private WPI_TalonFX backRight;
-    private DifferentialDrive wheels;
+    private WPI_TalonSRX frontLeft;
+    private WPI_TalonSRX backLeft;
+    private WPI_TalonSRX frontRight;
+    private WPI_TalonSRX backRight;
+    private MecanumDrive wheels;
     private boolean inverseState;
 
     public Wheels(int fL, int bL, int fR, int bR) {
         //initialize motor objects
-        frontLeft = new WPI_TalonFX(fL);
-        backLeft = new WPI_TalonFX(bL);
-        frontRight = new WPI_TalonFX(fR);
-        backRight = new WPI_TalonFX(bR);
+        frontLeft = new WPI_TalonSRX(fL);
+        backLeft = new WPI_TalonSRX(bL);
+        frontRight = new WPI_TalonSRX(fR);
+        backRight = new WPI_TalonSRX(bR);
         //so motors brake when speed is 0
         frontRight.setNeutralMode(NeutralMode.Brake);
         frontLeft.setNeutralMode(NeutralMode.Brake);
@@ -34,65 +34,20 @@ public class Wheels {
         backLeft.setNeutralMode(NeutralMode.Brake);
         
         inverseState = false;
-        wheels = new DifferentialDrive(new SpeedControllerGroup(frontLeft, backLeft), new SpeedControllerGroup(frontRight, backRight));
+        wheels = new MecanumDrive(
+            frontLeft, 
+            backLeft, 
+            frontRight,
+            backLeft);
     }
 
-    public double getRotations(String location) { 
-        if (location == "fL") {
-            double temp = frontLeft.getSensorCollection().getIntegratedSensorPosition();
-            double val = temp / 2048;
-            return val;
-        }
-        if (location == "bR") {
-            double temp = backRight.getSensorCollection().getIntegratedSensorPosition();
-            double val = temp / 2048;
-            return val;
-            
-        }
-        return 0;
-
-    }
-
-    public void resetRotations() {
-        frontLeft.setSelectedSensorPosition(0);
-        backRight.setSelectedSensorPosition(0);
-    }
     // Negative speed turns wheels backwards
-    public void drive(double leftSpeed, double rightSpeed) {
+    public void drive(double xSpeed, double ySpeed, double rotationSpeed) {
 
-        leftSpeed *= 0.8;
-        rightSpeed *= 0.8;
-        if(!inverseState)
-        {
-            //if inversState is true, reverse the speeds and call drive 
-            frontLeft.set(leftSpeed);
-            backLeft.set(leftSpeed);
-            frontRight.set(-rightSpeed);
-            backRight.set(-rightSpeed);
-
-        }
-        else
-        {
-            //makes sure speeds are positive
-            //wheels.tankDrive(Math.abs(leftSpeed), Math.abs(rightSpeed));
-            frontLeft.set(-leftSpeed);
-            backLeft.set(-leftSpeed);
-            frontRight.set(rightSpeed);
-            backRight.set(rightSpeed);
-       
-        }
+        wheels.driveCartesian(ySpeed, xSpeed, rotationSpeed);
     }
 
-    public void diffDrive(double speed1, double speed2, DriveType dType) {
-        switch(dType) {
-            case ARCADE:
-                wheels.arcadeDrive(speed1 * 0.8, speed2 * 0.8); // speed scaling may need to be adjusted as we can't test in person right now
-                break;
-            case TANK:
-                wheels.tankDrive(speed1 * 0.9, speed2 * 0.9);
-                break;
-        }
-    }
+   
 
     public void inverse()
     {
